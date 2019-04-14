6d23a31381c5889e800c45000f57158e2e4f0b52
郭qh <gqhflying@hotmail.com>
2019-04-15 00:51:26 +0800

    add permission

---
+++ src/DataCenterOperation/Site/Controllers/BaseController.cs
@@ -0,0 +1,40 @@
+﻿using DataCenterOperation.Data.Entities;
+using DataCenterOperation.Services;
+using DataCenterOperation.Site.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataCenterOperation.Site.Controllers
+{
+    public class BaseController : Controller
+    {
+        private readonly IUserService _userService;
+        private readonly string _userClaimName = "userid";
+
+        public BaseController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        protected async Task<UserAccessViewModel> GetUser()
+        {
+            UserAccessViewModel loginUser = new UserAccessViewModel();
+
+            ClaimsPrincipal claimsPrincipal = this.HttpContext.User;
+            if (claimsPrincipal != null && claimsPrincipal.Identity.IsAuthenticated)
+            {
+                Claim claim = claimsPrincipal.FindFirst(_userClaimName);
+                var user = await _userService.GetUserAsync(new Guid(claim.Value));
+
+                if (user != null)
+                {
+                    loginUser = UserAccessViewModel.GetUserAcessInfo(user);
+                }
+            }
+            return loginUser;
+        }
+    }
+}
---
+++ src/DataCenterOperation/Site/ViewModels/UserAccessViewModel.cs
@@ -0,0 +1,45 @@
+﻿using DataCenterOperation.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataCenterOperation.Site.ViewModels
+{
+    public enum UserPermission
+    {
+        UnKnow,
+        Admin,
+        Normal
+    }
+
+    public class UserAccessViewModel
+    {
+        public User User { get; set; }
+
+        public UserPermission UserPermission { get; set; }
+
+        public static UserAccessViewModel GetUserAcessInfo(User user)
+        {
+            UserAccessViewModel accessViewModel = new UserAccessViewModel();
+
+            accessViewModel.User = user;
+
+            UserPermissionMapping(accessViewModel);
+
+            return accessViewModel;
+        }
+
+        public static void UserPermissionMapping(UserAccessViewModel accessViewModel)
+        {
+            if (accessViewModel.User.IsAdmin)
+            {
+                accessViewModel.UserPermission = UserPermission.Admin;
+            }
+            else
+            {
+                accessViewModel.UserPermission = UserPermission.Normal;
+            }
+        }
+    }
+}
